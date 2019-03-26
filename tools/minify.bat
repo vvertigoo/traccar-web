757540f751fe008646d818fb684afd254ac0fd1b
Vadym Prosianiuk <vvertigoo2602@gmail.com>
2019-03-26 20:00:28 +0200

    Add prod changes

--- tools/minify.bat
+++ tools/minify.bat
@@ -1,5 +1,5 @@
 @echo off
-cd c:\Projects\traccar\traccar-web\
-set SDK=c:\Users\vvertigoo\bin\Sencha\Cmd\6.7.0.37
+cd c:\Projects\traccar\traccar-web\web
+set SDK=c:\Projects\extjs\
 
-c:\Users\vvertigoo\bin\Sencha\Cmd\sencha -sdk %SDK% compile -classpath=app.js,app,%SDK%\packages\core\src,%SDK%\packages\core\overrides,%SDK%\classic\classic\src,%SDK%\classic\classic\overrides exclude -all and include -recursive -file app.js and exclude -namespace=Ext and concatenate -closure app.min.js
+C:\Users\vvertigoo\bin\Sencha\Cmd\6.7.0.37\sencha -sdk %SDK% compile -classpath=app.js,app,%SDK%\packages\core\src,%SDK%\packages\core\overrides,%SDK%\classic\classic\src,%SDK%\classic\classic\overrides exclude -all and include -recursive -file app.js and exclude -namespace=Ext and concatenate -closure app.min.js
